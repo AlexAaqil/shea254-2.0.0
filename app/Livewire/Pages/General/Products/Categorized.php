383879d0cb5576a460af14093d511439cc5be433
AlexAaqil <alexaaqil.se@gmail.com>
2025-09-25 12:41:11 +0300

    fixed the image reload issues on categorized pages when adding to cart.

--- app/Livewire/Pages/General/Products/Categorized.php
+++ app/Livewire/Pages/General/Products/Categorized.php
@@ -16,11 +16,6 @@ class Categorized extends Component
     {
         $this->category = ProductCategory::where('slug', $category)->firstOrFail();
         $this->categories = ProductCategory::where('slug', '!=', $category)->get();
-        $this->products = $this->category->products()
-            ->with(['product_category', 'product_images'])
-            ->where('is_visible', true)
-            ->orderBy('title')
-            ->get();
     }
 
     public function addToCart(int $product_id): void
@@ -34,6 +29,12 @@ class Categorized extends Component
 
     public function render()
     {
-        return view('livewire.pages.general.products.categorized')->layout('layouts.guest');
+        $products = $this->category->products()
+            ->with(['product_category', 'product_images'])
+            ->where('is_visible', true)
+            ->orderBy('title')
+            ->get();
+        
+        return view('livewire.pages.general.products.categorized', compact('products'))->layout('layouts.guest');
     }
 }
