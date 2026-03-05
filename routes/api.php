96a2f5f8099b48a752474e2f0440a63714f50cc7
alexaaqil <alexaaqil.se@gmail.com>
2026-03-05 21:51:17 +0300

    added the paystack routes.

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Payments\KCBMpesaExpressController;
+use App\Http\Controllers\Sales\SaleController;
 
 Route::middleware('auth:sanctum')->get('user', function (Request $request) {
     return $request->user();
 });
 
 Route::post('payment/callback', [KCBMpesaExpressController::class, 'handleCallback'])->name('payments.kcb-callback');
+
+Route::get('/paystack/callback', [SaleController::class, 'paystackCallback'])->name('paystack.callback');
+Route::post('/paystack/webhook', [SaleController::class, 'paystackWebhook'])->name('paystack.webhook');
