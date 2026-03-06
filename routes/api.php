452d7687279eac64f5c06b9fa6c28b212bdc0490
alexaaqil <alexaaqil.se@gmail.com>
2026-03-07 02:39:20 +0300

    debugging: corrected the paystack controller namespace.

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Payments\KCBMpesaExpressController;
-use App\Http\Controllers\Payments\PayStackController;
+use App\Http\Controllers\Payments\PaystackController;
 
 Route::middleware('auth:sanctum')->get('user', function (Request $request) {
     return $request->user();
@@ -11,5 +11,5 @@ Route::middleware('auth:sanctum')->get('user', function (Request $request) {
 
 Route::post('payment/callback', [KCBMpesaExpressController::class, 'handleCallback'])->name('payments.kcb-callback');
 
-Route::get('/paystack/callback', [PayStackController::class, 'handleCallback'])->name('paystack.callback');
-Route::post('/paystack/webhook', [PayStackController::class, 'handleWebhook'])->name('paystack.webhook');
+Route::get('/paystack/callback', [PaystackController::class, 'handleCallback'])->name('paystack.callback');
+Route::post('/paystack/webhook', [PaystackController::class, 'handleWebhook'])->name('paystack.webhook');
